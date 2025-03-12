0ad80f68db3b0554ea74a0ad784f6a952481f89a
jiayi <jiayi031209@gmail.com>
2025-03-12 17:43:59 +0800

    Restructured directory

---
+++ training/ann.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class ANN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(ANN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
